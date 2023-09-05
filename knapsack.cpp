6a08401c63b32cbcfbf5f7adf81d7c1281ec9583
Nanditha403 <136417011+Nanditha403@users.noreply.github.com>
2023-09-05 10:46:51 +0530

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,26 +1,47 @@
-
+/* A Naive recursive implementation of
+0-1 Knapsack problem */
 #include <bits/stdc++.h>
 using namespace std;
 
+// A utility function that returns
+// maximum of two integers
+int max(int a, int b) { return (a > b) ? a : b; }
 
-int binomialCoeff(int n, int k)
+// Returns the maximum value that
+// can be put in a knapsack of capacity W
+int knapSack(int W, int wt[], int val[], int n)
 {
 
-	if (k > n)
+	// Base Case
+	if (n == 0 || W == 0)
 		return 0;
-	if (k == 0 || k == n)
-		return 1;
 
+	// If weight of the nth item is more
+	// than Knapsack capacity W, then
+	// this item cannot be included
+	// in the optimal solution
+	if (wt[n - 1] > W)
+		return knapSack(W, wt, val, n - 1);
 
-	return binomialCoeff(n - 1, k - 1)
-		+ binomialCoeff(n - 1, k);
+	// Return the maximum of two cases:
+	// (1) nth item included
+	// (2) not included
+	else
+		return max(
+			val[n - 1]
+				+ knapSack(W - wt[n - 1], wt, val, n - 1),
+			knapSack(W, wt, val, n - 1));
 }
 
-
+// Driver code
 int main()
 {
-	int n = 5, k = 2;
-	cout << "Value of C(" << n << ", " << k << ") is "
-		<< binomialCoeff(n, k);
+	int profit[] = { 60, 100, 120 };
+	int weight[] = { 10, 20, 30 };
+	int W = 50;
+	int n = sizeof(profit) / sizeof(profit[0]);
+	cout << knapSack(W, weight, profit, n);
 	return 0;
 }
+
+// This code is contributed by rathbhupendra

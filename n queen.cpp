d6251c088c0f1764dc4206c1f6730faa2cba453f
Nanditha403 <136417011+Nanditha403@users.noreply.github.com>
2023-09-05 10:49:47 +0530

    Update n queen.cpp

--- n queen.cpp
+++ n queen.cpp
@@ -1,26 +1,91 @@
 
+
 #include <bits/stdc++.h>
+#define N 4
 using namespace std;
 
-
-int binomialCoeff(int n, int k)
+void printSolution(int board[N][N])
 {
-
-	if (k > n)
-		return 0;
-	if (k == 0 || k == n)
-		return 1;
-
-
-	return binomialCoeff(n - 1, k - 1)
-		+ binomialCoeff(n - 1, k);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++)
+		if(board[i][j])
+			cout << "Q ";
+		else cout<<". ";
+		printf("\n");
+	}
 }
 
+
+bool isSafe(int board[N][N], int row, int col)
+{
+	int i, j;
+
+	
+	for (i = 0; i < col; i++)
+		if (board[row][i])
+			return false;
+
+	
+	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+		if (board[i][j])
+			return false;
+
+	for (i = row, j = col; j >= 0 && i < N; i++, j--)
+		if (board[i][j])
+			return false;
+
+	return true;
+}
+
+
+bool solveNQUtil(int board[N][N], int col)
+{
+
+	if (col >= N)
+		return true;
+
+	
+	for (int i = 0; i < N; i++) {
+		
+		
+		if (isSafe(board, i, col)) {
+			
+		
+			board[i][col] = 1;
+
+			
+			if (solveNQUtil(board, col + 1))
+				return true;
+
+			
+			board[i][col] = 0; // BACKTRACK
+		}
+	}
+
+	
+	return false;
+}
+
+
+bool solveNQ()
+{
+	int board[N][N] = { { 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 },
+						{ 0, 0, 0, 0 } };
+
+	if (solveNQUtil(board, 0) == false) {
+		cout << "Solution does not exist";
+		return false;
+	}
+
+	printSolution(board);
+	return true;
+}
 
 int main()
 {
-	int n = 5, k = 2;
-	cout << "Value of C(" << n << ", " << k << ") is "
-		<< binomialCoeff(n, k);
+	solveNQ();
 	return 0;
 }
+
